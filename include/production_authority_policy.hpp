f3a47701c4fd6900f876fe59253539ac89cbda7b
Mykola Bezruchko <tonybarannn@gmail.com>
2026-09-11 11:29:31 +0300

    feat: add production authority derivation contract

---
+++ include/production_authority_policy.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "production_authority_context.hpp"
+
+#include <optional>
+#include <utility>
+
+namespace AdaptiveMesh {
+
+class ProductionAuthorityDerivationResult final {
+public:
+    [[nodiscard]] ProductionAuthorityDecision decision() const noexcept {
+        return decision_;
+    }
+
+    [[nodiscard]]
+    ProductionAuthorityRejectionReason rejectionReason() const noexcept {
+        return rejectionReason_;
+    }
+
+    [[nodiscard]] bool hasCapability() const noexcept {
+        return capability_.has_value();
+    }
+
+    [[nodiscard]]
+    std::optional<ProductionExecutionCapability> takeCapability() && noexcept {
+        return std::move(capability_);
+    }
+
+private:
+    ProductionAuthorityDecision decision_ = ProductionAuthorityDecision::denied;
+    ProductionAuthorityRejectionReason rejectionReason_ =
+        ProductionAuthorityRejectionReason::authority_policy_denied;
+    std::optional<ProductionExecutionCapability> capability_;
+
+    friend class ProductionAuthorityDerivationPolicy;
+};
+
+class ProductionAuthorityDerivationPolicy final {
+public:
+    [[nodiscard]]
+    ProductionAuthorityDerivationResult evaluate(
+        const ProductionTransitionEligibilityDecision& eligibility,
+        const ProductionAuthorityContext& authorityContext) const noexcept;
+};
+
+} // namespace AdaptiveMesh
