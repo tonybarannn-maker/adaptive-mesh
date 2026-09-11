366b1d396d54574f79337cb5aa5513481a186e8a
Mykola Bezruchko <tonybarannn@gmail.com>
2026-09-11 18:50:56 +0300

    test: leave evaluator smoke test mesh-focused

--- tests/production_transition_evaluator_tests.cpp
+++ tests/production_transition_evaluator_tests.cpp
@@ -3,7 +3,6 @@
 using AdaptiveMesh::test_support::LiveScenario;
 using AdaptiveMesh::test_support::runLiveScenario;
 int main() {
-    return runLiveScenario(LiveScenario::fail_closed_binding) &&
-                   runLiveScenario(LiveScenario::persistence_change_detection)
+    return runLiveScenario(LiveScenario::fail_closed_binding)
         ? EXIT_SUCCESS : EXIT_FAILURE;
 }
