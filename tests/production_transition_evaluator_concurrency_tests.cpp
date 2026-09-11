d4cf020236ef048d2181a2c9581d5727507de0e5
Mykola Bezruchko <tonybarannn@gmail.com>
2026-09-11 19:39:45 +0300

    test: remove stale invalidate-and-drain wrapper

--- tests/production_transition_evaluator_concurrency_tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "internal/production_transition_evaluator_live_scenarios.hpp"
-#include <cstdlib>
-int main() {
-    return AdaptiveMesh::test_support::runLiveScenario(
-        AdaptiveMesh::test_support::LiveScenario::invalidate_and_drain)
-        ? EXIT_SUCCESS : EXIT_FAILURE;
-}
