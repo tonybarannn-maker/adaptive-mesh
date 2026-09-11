b5fe9ae92036e73866de5e99e239d01e5b57946b
Mykola Bezruchko <tonybarannn@gmail.com>
2026-09-11 11:29:58 +0300

    test: reject production capability copy

---
+++ tests/compile_fail/b5_nc02_capability_copy.cpp
@@ -0,0 +1,8 @@
+#include "production_authority_types.hpp"
+
+void probe(const AdaptiveMesh::ProductionExecutionCapability& source) {
+    AdaptiveMesh::ProductionExecutionCapability copy{source};
+    (void)copy;
+}
+
+int main() { return 0; }
