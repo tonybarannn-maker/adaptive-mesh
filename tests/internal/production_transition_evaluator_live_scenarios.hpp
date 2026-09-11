76f3ca4a9911a6165de3a3f567ef68980c90cd68
Mykola Bezruchko <tonybarannn@gmail.com>
2026-09-11 18:50:50 +0300

    test: remove component persistence case from live registry

--- tests/internal/production_transition_evaluator_live_scenarios.hpp
+++ tests/internal/production_transition_evaluator_live_scenarios.hpp
@@ -6,7 +6,6 @@ namespace AdaptiveMesh::test_support {
 
 enum class LiveScenario : std::uint8_t {
     fail_closed_binding,
-    persistence_change_detection,
     unchanged_persistence_current,
     changed_persistence_stale,
     persistence_anti_resurrection,
